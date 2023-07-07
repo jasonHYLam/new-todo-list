20875ea4b9aecea08c44f9b0946f493a31fad882
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-07 14:45:04 +0100

    Create createTodo()
    Factory function to create todo objects. Contains title, description,
    dueDate and priority. Includes get and set functions. Test with
    testTodo().

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,27 @@
-const makeTodo = () => {
+const makeTodo = (title, description, dueDate, priority) => {
+    let title = title;
+    let description = description;
+    let dueDate = dueDate;
+    let priority = priority;
 
+    const getProp = (prop) => prop;
+    const setProp = (prop, newValue) => prop = newValue;
+
+    return {getProp, setProp};
 }
+
+const todoTest = () => {
+    let test = makeTodo(
+        'let',
+        'let us',
+        'now',
+        'urgent',
+         )
+    console.log(
+
+    test.getProp(title)
+    )
+    
+}
+
+export {todoTest};
