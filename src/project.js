8701b4eb9d30d3d70cd269d1b08d7a2586f5458b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-08 12:43:45 +0100

    Create Project Module With Project Class

---
+++ src/project.js
@@ -0,0 +1,11 @@
+let projectArray = [];
+
+class Project {
+    constructor(name) {
+        this.todoArray = [];
+        this.name = name;
+    }
+    // add todo to project
+    // delete todo from project
+    // change todo 
+}
