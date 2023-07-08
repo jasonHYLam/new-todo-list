1a91906350f68a85d2358b483b2042dab26147e0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-08 16:25:56 +0100

    Create FormHandler Module

---
+++ src/formHandler.js
@@ -0,0 +1,6 @@
+import { pubSub } from "./pubsub";
+
+export function formHandler() {
+    const todoForm = document.querySelector("#todoForm")
+    console.log(todoForm)
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { todo } from "./todo";
 import { project } from "./project";
+import { formHandler } from ".formHandler"
 
 
 project();
 todo();
+formHandler();
