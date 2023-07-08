dbcd30b7d46aeea9b5561876981e595eb4c0c190
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-08 16:35:51 +0100

    Create Display Module

--- src/formHandler.js
+++ src/formHandler.js
@@ -3,4 +3,12 @@ import { pubSub } from "./pubsub";
 export function formHandler() {
     const todoForm = document.querySelector("#todoForm")
     console.log(todoForm)
+    const projectForm = document.querySelector("#projectForm")
+    console.log(projectForm)
+
+    const submitProject = document.querySelector('#projectSubmit')
+    submitProject.addEventListener("click", () => {
+        // pubSub('createProject', console.log(document.querySelector('#projectForm input').value))
+        console.log(document.querySelector('#projectForm input').value)
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { todo } from "./todo";
 import { project } from "./project";
-import { formHandler } from ".formHandler"
+import { formHandler } from "./formHandler";
 
 
 project();
