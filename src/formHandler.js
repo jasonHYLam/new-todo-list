f924537ba92d8b7b1958a4dea2138ae8df5c3675
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-08 22:43:03 +0100

    Refactor Todo Creation Argument To Array

--- src/formHandler.js
+++ src/formHandler.js
@@ -4,14 +4,20 @@ export function formHandler() {
     const todoForm = document.querySelector("#todoForm")
     console.log(todoForm)
     const submitTodo = document.querySelector("#todoSubmit");
+
+    const getTodoArgs = () => {
     const todoTitle = document.querySelector("#todoTitle").value;
     const todoDescription = document.querySelector("#todoDescription").value;
     const todoDueDate = document.querySelector("#todoSubmit").value;
     const todoPriority = document.querySelector("#todoSubmit").value;
+    const todoArgs = [todoTitle, todoDescription, todoDueDate, todoPriority]
+    return todoArgs
+    }
     submitTodo.addEventListener("click", (e) => {
         e.preventDefault();
+        console.log(`title is ${todoTitle}`)
         // pubsub for creating a new todo
-        pubSub.publish('todoFormSubmitted', todoTitle, todoDescription, todoDueDate, todoPriority);
+        pubSub.publish('todoFormSubmitted', getTodoArgs());
 
     })
 
