23749464c66bcc457234a1e75a2e75e910fa83ab
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-08 13:39:15 +0100

    Add Console Log Messages To PubSub Functions

--- src/pubsub.js
+++ src/pubsub.js
@@ -3,17 +3,20 @@ export const pubSub = {
 
     subscribe: function(event, cb) {
         // what does this.events[event] look like?
+        console.log(`something subscribed to know about ${event}`)
         this.events[event] = this.events[event]|| [];
         this.events[event].push(cb);
     },
 
     unsubscribe: function(event, cb) {
+        console.log(`something unsubscribed from ${event}`)
         if (this.events[event]) {
             this.events[event] = this.events[event].filter(f => f != cb)
         }
     },
 
     publish: function(event, data) {
+        console.log(`making a broadcast about ${event} with data`);
         if (this.events[event]) {
             this.events[event].forEach(f => {
                 f(data);
