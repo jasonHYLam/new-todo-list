89fe992e8ce52ed73d5eb7596095864b650b1c36
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-10 09:07:40 +0100

    Create Variable To Store Matching Project
    Store matching project in matchingProject, in project.js module.

--- src/project.js
+++ src/project.js
@@ -6,11 +6,22 @@ export function project() {
     pubSub.subscribe('getProject',(projectMatch) => {//dunno what to call projectMatch; the thing that i pass to find the correct project
         const selectedProject = projectArray.filter((project) => project.name == projectMatch)//also, may need to use data attribute, rather than name. or use form vaolidation to not use same name
 
+        // oh boy what do i do here, because the function i want to call has two arguments that come from different places...
+        // need a todo, should i just send it here first
+        // and then put it in addTodoToProjectArray
+        console.log('does THIS occur')
 
         // pubSub to display the todos; send to display.js
-        pubSub.publish("displaySelectedProjectElements", selectedProject);
+
+        // deal with this later
+        // pubSub.publish("displaySelectedProjectElements", selectedProject);
 
     });
+
+    // where do i put this frickin thing
+        pubSub.subscribe('sendTodoToProjectTodoArray', (todo) => {
+            console.log(todo)
+        });
 
     // subscribe to when projectform is submitted
     pubSub.subscribe('projectFormSubmitted', (name) => {
@@ -22,11 +33,16 @@ export function project() {
 
     // need to get a project first
     // add to a project's todoArray
-    pubSub.subscribe()
+    // pubSub.subscribe()
 
 
 
     let projectArray = [];
+
+
+    const addTodoToProjectArray = (project, todo) => {
+        project.todoArray.push(todo)
+    }
 
     const increaseProjects = (project) => {
         projectArray.push(project)
--- src/todo.js
+++ src/todo.js
@@ -8,7 +8,11 @@ function todo() {
     // pubsub SUBSCRIBE
     pubSub.subscribe('todoFormSubmitted', (args) => {
         const newTodo = new Todo(args[0], args[1], args[2], args[3])
+
         pubSub.publish('newTodoCreated',todoArray) //send to display.js ; also, this is the wrong todo array... i need to send this to a project array.
+
+        // in fact, what i think needs to happen is to publish the todo to an event, and project will subscribe to put that in todoarray.
+        // and then that will publish to display.
         // NEXT, IMPLEMENT CHANGING TODOS; CHANGE PROPERTY, DELETE
     })
 
@@ -31,6 +35,9 @@ function todo() {
         this.priority = priority;
         // when todo is created, push to todoarray
         pushToArray(this);
+
+        // may have to move this elsewhere, but when new todo is created, append it to a project todoArray
+        pubSub.publish('sendTodoToProjectTodoArray', this)
         }
 
         setProp(prop, value) {
